8c4591542cdc08cb9838fa0fc36acbd2d0fd0dc4
Tommy Ngo <phuoc.ngo@vanderbilt.edu>
2024-06-05 22:44:00 -0700

    Initial Commit

---
+++ SwiftUI-Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Demo
+//
+//  Created by Phuoc Ngo on 6/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Demo/SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DemoApp.swift
+//  SwiftUI-Demo
+//
+//  Created by Phuoc Ngo on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
